1745aa433fc29b520d32b19f94c4b78a359182d9
oeda <m.ooeda@eisansystem.jp>
2020-12-19 16:50:22 +0900

    second

--- DataGridView/Form1.Designer.cs
+++ DataGridView/Form1.Designer.cs
@@ -28,13 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 38);
+            this.dataGridView1.Name = "dataGridView1";
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(767, 334);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- DataGridView/Form1.cs
+++ DataGridView/Form1.cs
@@ -16,5 +16,81 @@ namespace DataGridView
         {
             InitializeComponent();
         }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            aaa();
+
+            return;
+
+            //this.dataGridView1.ColumnCount = 3;
+            //this.dataGridView1.Columns[0].ReadOnly = true;
+            //this.dataGridView1.Columns[1].ReadOnly = true;
+            //this.dataGridView1.Columns[2].ReadOnly = true;
+            //this.dataGridView1.Columns[0].Width = 300;
+            //this.dataGridView1.Columns[1].Width = 200;
+            //this.dataGridView1.Columns[2].Width = 200;
+            //this.dataGridView1.RowCount = 5;
+
+
+            //DataGridViewComboBoxColumn a = new DataGridViewComboBoxColumn();
+            //DataGridViewComboBoxCell c = new DataGridViewComboBoxCell();
+            //c.Value = 100;
+            
+            //a.Name = "100";
+            //a.HeaderText = "aaaa";
+
+            //((DataGridViewComboBoxCell)dataGridView1[1, 2]).co
+
+            //((DataGridViewComboBoxCell)dataGridView1[1, 4]).Items.Add(param.Name);
+        }
+
+        private void aaa()
+        {
+            DataTable dt = new DataTable();
+            DataRow dr;
+
+            //テーブルに列追加
+            dt.Columns.Add("コード");
+            dt.Columns.Add("曜日");
+
+            //テーブルに曜日をセット
+            dr = dt.NewRow();
+            dr["コード"] = "1";
+            dr["曜日"] = "月曜日";
+            dt.Rows.Add(dr);
+
+            dr = dt.NewRow();
+            dr["コード"] = "2";
+            dr["曜日"] = "火曜日";
+            dt.Rows.Add(dr);
+
+            dr = dt.NewRow();
+            dr["コード"] = "3";
+            dr["曜日"] = "水曜日";
+            dt.Rows.Add(dr);
+
+            dr = dt.NewRow();
+            dr["コード"] = "4";
+            dr["曜日"] = "木曜日";
+            dt.Rows.Add(dr);
+
+            dr = dt.NewRow();
+            dr["コード"] = "5";
+            dr["曜日"] = "金曜日";
+            dt.Rows.Add(dr);
+
+            //曜日カラムをデータグリッドビューにセット
+            DataGridViewComboBoxColumn colDate = new DataGridViewComboBoxColumn();
+            colDate.DataPropertyName = "Date";
+            colDate.DataSource = dt;
+            colDate.ValueMember = "コード";
+            colDate.DisplayMember = "曜日";
+            dataGridView1.Columns.Insert(0, colDate);
+            colDate.Name = "曜日";
+        }
     }
+    
 }
